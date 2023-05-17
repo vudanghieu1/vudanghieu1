e5ceeefe29f47e01b5c922373278b64994f85003
Hiếu <55701970+vudanghieu1@users.noreply.github.com>
2023-05-17 22:39:51 +0700

    upload file

--- main.py
+++ main.py
@@ -5,41 +5,31 @@ import cv2
 import train
 
 
-image = cv2.imread("cccd10.jpg")
+image = cv2.imread("cccd6.jpg")
 image = cv2.resize(image, (1280, 960))
 
-# resize image smaller
-# scale_percent = 50
-# width = int(image.shape[1] * scale_percent / 100)
-# height = int(image.shape[0] * scale_percent / 100)
-# dsize = (width, height)
-# image = cv2.resize(image, dsize)
-# print(image.shape[1], image.shape[0])
-
-
-# đổi dang ảnh xám
+# Đổi dang ảnh xám
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# ảnh nhị phân
+# Ẩnh nhị phân
 thresh_img = cv2.threshold(
     gray, 135, 255, cv2.THRESH_BINARY)[1]
 
-# làm mờ
+# Làm mờ
 gray = cv2.medianBlur(gray, 5)
 
-
-# bọc viền
+# Phát hiện biên
 edges = cv2.Canny(gray, 50, 180)
-
 
 # Hiển thị ảnh kết quả
 cv2.imshow('Gray', gray)
 cv2.imshow('ed', edges)
 cv2.imshow('thresh_img', thresh_img)
 
+
 # Tìm các đường viền
-contours, hierarchy = cv2.findContours(
-    edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = cv2.findContours(
+    edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
 
 # Tìm hình chữ nhật bao quanh đối tượng
@@ -56,8 +46,6 @@ for contour in contours:
             continue
         arr_ID.append([x, y, w, h])
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # cv2.rectangle(image, (500, 300), (1200, 470), (0, 255, 0), 2)
 
 
 # sắp xếp thứ tự ảnh lần lượt từ trái qua phải
